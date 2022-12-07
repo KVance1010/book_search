521d2531a24957fca9b5d4518009a42e4471bb5f
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-12-06 21:24:32 -0800

    fixed login and signup

---
+++ client/build/precache-manifest.d7f8966290cae6475982021da9bc4479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6272d2d4c2d9f760d51117bfcae47086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f70c7f0b920b4af8e44e",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "b468be072bd9a32410a5",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "f70c7f0b920b4af8e44e",
+    "url": "/static/js/2.749cd201.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.749cd201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b468be072bd9a32410a5",
+    "url": "/static/js/main.c87e1542.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c87e1542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
