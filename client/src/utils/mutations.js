2e6e01e2ce92cb5d68e76918f4c057d615c17f91
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-12-06 20:25:34 -0800

    checked auth file in the backend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,7 +15,6 @@ export const LOGIN_USER = gql`
 export const CREATE_USER = gql`
   mutation createUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-      token
       user {
         _id
         username
